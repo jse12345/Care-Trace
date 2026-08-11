2e4c4485cf7048528cfe0f5da4db73b3b09a40c9
apollochocolate <165468301+apollochocolate@users.noreply.github.com>
2026-08-11 15:19:28 +0900

    api.jsx 외부 ip 허용, vite.config.js의 DB ip 변경

--- CareTrace_front/vite.config.js
+++ CareTrace_front/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 0.0.0.0 - 같은 네트워크(AP)의 다른 PC에서 접속 허용
+    port: 5173
+  }
 })
