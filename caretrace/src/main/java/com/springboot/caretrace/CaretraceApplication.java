fdac0c2686b0ff7289315338195b08248f234021
jse12345 <used776655@gmail.com>
2026-08-11 10:27:52 +0900

    초기프로젝트

---
+++ caretrace/src/main/java/com/springboot/caretrace/CaretraceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.caretrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaretraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaretraceApplication.class, args);
+    }
+
+}
